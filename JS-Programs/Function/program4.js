30b498bd5b76938184f1192deb8ff24895bad0b8
Nityagopaljena98 <nityagjena@hotmail.com>
2025-10-12 03:17:54 +0530

    returns the index of a given value in an array

---
+++ JS-Programs/Function/program4.js
@@ -0,0 +1,20 @@
+
+/*
+
+    Q. Write a function that returns the index of a given value in an array. If not found, return -1.
+
+*/
+
+
+function returnIndexNo() {
+    let arr = [1, 3, 4, 10, , 35, 7, 70, 9, 10];
+    let value = 0;
+    for (let i = 0; i < arr.length; i++) {
+        if (arr[i] == value) {
+            return i;
+        }
+    }
+    return -1;
+}
+
+console.log('Index No is:', returnIndexNo());
