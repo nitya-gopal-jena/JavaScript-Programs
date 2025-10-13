8a1c2312b24e908995325db804dc454d005351b8
Nityagopaljena98 <nityagjena@hotmail.com>
2025-10-13 22:27:24 +0530

    check if an array is sorted in ascending order or not

--- JS-Programs/Function/program3.js
+++ JS-Programs/Function/program3.js
@@ -4,8 +4,18 @@
 
 */
 
+
+
+
+// Import prompt-sync package to work with prompt() in Node.js environment.
+const prompt = require('prompt-sync')();
+
 function ascendingOrder() {
-    let arr = [10, 20, 30, 40, 50];
+    // let arr = [10, 20, 30, 40, 50];
+    
+    let arr = prompt('Enter array element separated by commas:  ')
+    arr = arr.split(',').map(Number);
+
     for (let i = 0; i < arr.length - 1; i++) {
         if (arr[i] > (arr[i + 1])) {
             console.log('Not in ascending order', arr);
