553bc5250e29789bc8845785340a3e13295a230b
Nityagopaljena98 <nityagjena@hotmail.com>
2025-10-13 22:22:19 +0530

    returns the largest number from an array

--- JS-Programs/Function/program2.js
+++ JS-Programs/Function/program2.js
@@ -7,9 +7,14 @@
 
 
 //Without Method
+// Import prompt-sync package to work with prompt() in Node.js environment.
+const prompt = require('prompt-sync')();
+
 function largestNumber() {
 
-    let arr = [10, 20, 30, 40, 50, 60, 70, 80, 90, 100];
+    // let arr = [10, 20, 30, 40, 50, 60, 70, 80, 90, 100]; 
+    let arr = prompt('Enter array element separated by commas:  ')
+    arr = arr.split(',').map(Number);
     let largest = arr[0];
 
     for (let i = 1; i < arr.length; i++) {
