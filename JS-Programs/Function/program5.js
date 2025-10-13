39c3c4476fba647ec29c9a52eb8af14f1d4a642b
Nityagopaljena98 <nityagjena@hotmail.com>
2025-10-13 22:32:46 +0530

    reversed an array elements

--- JS-Programs/Function/program5.js
+++ JS-Programs/Function/program5.js
@@ -6,8 +6,15 @@
 */
 
 
+// Import prompt-sync package to work with prompt() in Node.js environment.
+const prompt = require('prompt-sync')();
+
 function reverseArrayElements() {
-    let arr = [10, 20, 30, 40, 50, 60];
+    // let arr = [10, 20, 30, 40, 50, 60];
+
+     let arr = prompt('Enter array element separated by commas:  ')
+    arr = arr.split(',').map(Number);
+
     let reversed = [];
     let j = 0;
 
