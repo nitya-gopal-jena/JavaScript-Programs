1e02f15b4f29183f15c2328430c10fc7c6191193
Nityagopaljena98 <nityagjena@hotmail.com>
2025-10-14 19:09:07 +0530

    Remove the first element of an array using shift() and display the updated array

---
+++ JS-Programs/Array/array3.js
@@ -0,0 +1,13 @@
+
+
+/*
+    
+    Q. Remove the first element of an array using shift() and display the updated array.
+
+*/
+
+
+
+let colors = ["red", "green", "blue", "yellow"];
+colors.shift();
+console.log("Updated array:", colors);
