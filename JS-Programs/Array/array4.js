ac27f67e8cd3500620bfb28c55a2b1781936f528
Nityagopaljena98 <nityagjena@hotmail.com>
2025-10-14 19:09:36 +0530

    Find the sum of all elements in an array

---
+++ JS-Programs/Array/array4.js
@@ -0,0 +1,17 @@
+
+/*
+
+    Q. Find the sum of all elements in an array.
+
+*/
+
+
+
+
+let numbers = [10, 20, 30, 40, 50];
+let sum = 0;
+for (let i = 0; i < numbers.length; i++) {
+    sum += numbers[i];
+}
+
+console.log("Sum of all elements:", sum);
