79d159de9e36ee1ea13e515df9e33fe83da11711
Nityagopaljena98 <nityagjena@hotmail.com>
2025-10-14 19:07:53 +0530

    print all the elements in an array

---
+++ JS-Programs/Array/array1.js
@@ -0,0 +1,17 @@
+
+
+/*
+
+    Q. Write a program to create an array of numbers and print all the elements.
+
+*/
+
+
+
+let arr = [20, 30, 10, 60, 50];
+for (let i = 0; i < arr.length; i++){
+    console.log(arr[i]); 
+}
+
+
+
